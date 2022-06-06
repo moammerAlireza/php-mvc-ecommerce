8c923f2c5d4459ae87c5a6da29b260c2e9c2deb1
Alireza Moammer <moameralireza@gmail.com>
2022-06-06 14:17:15 +0430

    It was added RouteDispatcher

--- app/routing/routes.php
+++ app/routing/routes.php
@@ -2,17 +2,4 @@
 
 $router= new AltoRouter;
 
-$router->map('GET', '/', '', 'home');
-
-$match= $router->match();
-
-if($match){
-    require_once __DIR__ . '/../controllers/BaseController.php';
-    require_once __DIR__ . '/../controllers/IndexController.php';
-    $index = new \App\controllers\IndexController();
-    $index->show();
-}else{
-    header($_SERVER['SERVER_PROTOCOL']. '404 Not Found');
-    echo "Page not found";
-}
-
+$router->map('GET', '/', 'App\controllers\IndexController@show', 'home');
--- bootstrap/init.php
+++ bootstrap/init.php
@@ -1,7 +1,10 @@
 <?php
-
+// start session if not already started
 if(!isset($_SESSION)) session_start();
 
+// load environment variable
 require_once __DIR__.'/../app/config/_env.php';
 
 require_once __DIR__ .'/../app/routing/routes.php';
+
+new \App\RouteDispatcher($router);
