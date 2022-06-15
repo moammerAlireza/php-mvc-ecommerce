366614112269a19fa0c738cd891c6a88ff4b6f0f
Alireza Moammer <moameralireza@gmail.com>
2022-06-15 14:45:40 +0430

    It was added form request validation

--- app/controllers/admin/ProductCategoryController.php
+++ app/controllers/admin/ProductCategoryController.php
@@ -4,6 +4,8 @@ use App\models\Category;
 use App\classes\Request;
 use App\classes\CSRFToken;
 use Illuminate\Support\Facades\Session;
+use Dotenv\Validator;
+use App\classes\ValidateRequest;
 //use Illuminate\Support\Facades\Request;
 
 class ProductCategoryController
@@ -19,8 +21,20 @@ class ProductCategoryController
     {
         if (Request::has('post')){
             $request = Request::get('post');
-           
+            
             if(CSRFToken::VerifyCSRFToken($request->token)){
+
+                $rules = [
+                    'name' => ['required' => true, 'maxLength' => 5, 'string' => true, 'unique' => 'categories']
+                ];
+
+                $validate = new ValidateRequest;
+                $validate->abide($_POST, $rules);
+
+                if($validate->hasError()){
+                    var_dump($validate->getErrorMessage());
+                    exit;
+                }
                 //process form data
                 Category::create([
                     'name' => $request->name,
