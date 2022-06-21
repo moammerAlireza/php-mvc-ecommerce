4ccaf5071e57015288ba73730549da27f92cf070
Alireza Moammer <moameralireza@gmail.com>
2022-06-21 14:17:19 +0430

    It was added edit category form

--- resources/views/admin/products/categories.blade.php
+++ resources/views/admin/products/categories.blade.php
@@ -10,28 +10,28 @@
         </div>
 
        @include('includes.message')
-
+        
 
         <div class="row expanded">
-            <div class="small-6 medium-6 column" style="margin-right: 10%; margin-left: 10%">
+            <div class="small-12 medium-6 column" style="margin-right: 10%; margin-left: 10%">
                 <form action="" method="post">
                     <div class="input-group">
-                        <input type="text" class="input-group-field" name="name"
-                        placeholder="Category name">
-                        <input type="hidden" name="token" value="{{\App\classes\CSRFToken::_token()}}">
+                        <input type="text" class="input-group-field" placeholder="Search by Name">
+                       
                         <div class="input-group-button">
-                            <input type="submit" class="button" value="Create">
+                            <input type="submit" class="button" value="Search">
                         </div>
                     </div>
                 </form>
             </div>
             
-            <div class="small-6 medium-6 column"  style="margin-right: 10%; margin-left: 10%">
+            <div class="small-12 medium-5 end column"  style="margin-right: 10%; margin-left: 10%">
                 <form action="/admin/product/categories" method="post">
                     <div class="input-group">
-                        <input type="text" class="input-group-field" placeholder="Search by name">
+                        <input type="text" class="input-group-field" name="name" placeholder="Caregory Name">
+                        <input type="hidden" name="token" value="{{\App\classes\CSRFToken::_token()}}">
                         <div class="input-group-button">
-                            <input type="submit" class="button" value="search">
+                            <input type="submit" class="button" value="Create">
                         </div>
                     </div>
                 </form>
@@ -50,8 +50,26 @@
                                     <td>{{$category['slug']}}</td>
                                     <td>{{$category['added']}}</td>
                                     <td width="100" class="text-right">
-                                        <a href="#"><i class="fa fa-edit"></i></a>
+                                        <a data-open="item-{{$category['id']}}"><i class="fa fa-edit"></i></a>
                                         <a href="#"><i class="fa fa-times"></i></a>
+                                        
+                                        <!-- Edit category model-->
+                                        <div class="reveal" id="item-{{$category['id']}}" 
+                                        data-reveal data-close-on-click="false" data-close-on-esc="false">
+                                            <h1>Edit Category</h1>
+                                            <form>
+                                                <div class="input-group">
+                                                    <input type="text" name="name" value="{{$category['name']}}">
+                                                    <input type="hidden" name="token" value="{{\App\classes\CSRFToken::_token()}}">
+                                                    <div class="input-group-button">
+                                                        <input type="submit" class="button update-category" id="{{$category['id']}}" value="Update">
+                                                    </div>
+                                                </div>
+                                            </form>
+                                            <button class="close-button" data-close aria-label="Close modal" type="button">
+                                              <span aria-hidden="true">&times;</span>
+                                            </button>
+                                          </div>
                                     </td>
                                 </tr>
                                 
