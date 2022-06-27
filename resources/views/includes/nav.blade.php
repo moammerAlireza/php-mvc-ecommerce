367bd498aa0b2cd74c8eed41bc227828f213a70f
Alireza Moammer <moameralireza@gmail.com>
2022-06-27 13:44:58 +0430

    It was added Front Store Navigation Style

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,10 +1,11 @@
 <?php $categories= \App\Models\Category::with('subCategories')->get(); ?>
 
 <header class="navigation">
-    {{-- <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
-        <button class="menu-icon" type="button" data-toggle="example-menu"></button>
-        <div class="title-bar-title">Menu</div>
-      </div> --}}
+   <div class="hide-for-medium">
+         <div class="title-bar toggle" data-responsive-toggle="main-menu" data-hide-for="medium">
+        <button class="menu-icon float" type="button" data-toggle="main-menu"></button>
+        <a href="/" class="float-left small-logo">ACME</a>
+      </div>
       
       <div class="top-bar" id="main-menu">
         <div class="menu medium-horizontal expanded medium-text-center" data-dropdown-menu
@@ -12,12 +13,12 @@
             data-disable-hover="true" data-close-on-click-inside="false">
 
             <div class="top-bar-title show-for-medium">
-                <a href="/" class="logo">ACME Store</a>
+                <a href="/" class="logo"></a>
             </div>
             
                     <div class="top-bar-left">
                         <ul class="dropdown menu vertical medium-horizontal">
-                        <li> Acme products</li>
+                        <li><a href="#">Acme products</a></li>
                         @if (count($categories))
                             <li>
                                 <a href="#">Categories</a>
@@ -47,7 +48,7 @@
                 
                 <div class="top-bar-right">
                         <ul class="dropdown menu vertical medium-horizontal">
-                        <li> Username</li>
+                        <li><a href="#">Username</a></li>
                         <li><a href="#">Sign In</a></li>
                         <li><a href="#">Register</a></li>
                         <li><a href="#">Cart</a></li>
@@ -55,5 +56,59 @@
                 </div>
             
         </div>
+      </div>
+   </div>
+
+      <div class="show-for-medium">
+        <div class="top-bar" id="main-menu">
+            <div class="menu medium-horizontal expanded medium-text-center" data-dropdown-menu
+                data-responsive-menu="drilldown medium-dropdown" data-click-open="true"
+                data-disable-hover="true" data-close-on-click-inside="false">
+    
+                <div class="top-bar-title show-for-medium">
+                    <a href="/" class="logo"></a>
+                </div>
+                
+                        <div class="top-bar-left">
+                            <ul class="dropdown menu vertical medium-horizontal">
+                            <li> Acme products</li>
+                            @if (count($categories))
+                                <li>
+                                    <a href="#">Categories</a>
+                                    <ul class="menu vertical sub dropdown">
+                                        @foreach ($categories as $category)
+                                            <li>
+                                                <a href="#">{{$category->name}}</a>
+                                                @if (count($category->subCategories))
+                                                    <ul class="menu sub vertical">
+                                                        @foreach ($category->subCategories as $subCategory)
+                                                            <li>
+                                                                <a href="#">
+                                                                    {{$subCategory->name}}
+                                                                </a>
+                                                            </li>
+                                                        @endforeach
+    
+                                                    </ul>
+                                                @endif
+                                            </li>
+                                        @endforeach
+                                    </ul>
+                                </li>
+                            @endif
+                            </ul>
+                        </div>
+                    
+                    <div class="top-bar-right">
+                            <ul class="dropdown menu vertical medium-horizontal">
+                            <li> Username</li>
+                            <li><a href="#">Sign In</a></li>
+                            <li><a href="#">Register</a></li>
+                            <li><a href="#">Cart</a></li>
+                            </ul>
+                    </div>
+                
+            </div>
+          </div>
       </div>
 </header>
